31113c98b55d0c50df24c2ecbc287fb8073b50bb
RainPPR <PPR2125773894@163.com>
2025-11-01 20:57:07 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import os
 import toc
 import json
 import yaml
@@ -13,3 +14,7 @@ if __name__ == "__main__":
     template['nav'] = toc.get_site_nav()
     with open('mkdocs.yml', 'w', encoding='utf-8') as file:
         yaml.dump(template, file, allow_unicode=True, indent=4, sort_keys=False)
+    try:
+        os.system("git rev-parse --short HEAD | xargs -I % sed -i \"s/githash: ''/githash: '%'/g\" mkdocs.yml")
+    except Exception as e:
+        print(f"无法创建 githash.txt: {e}")
