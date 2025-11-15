40425a0b6bf37da9be74e5dacc3d0b888b08d7fe
RainPPR <PPR2125773894@163.com>
2025-11-15 22:17:12 +0800

    Update

--- generate.py
+++ generate.py
@@ -10,7 +10,8 @@ def load_json(file_path):
 
 if __name__ == "__main__":
     info = load_json('info.json')
-    template = info['project'] | toc.parse_yaml('script/template.yml')
+    template = info['project'] | toc.parse_yaml('script/template.yml') | toc.parse_yaml('docs/assets/extra.yml')
+    template['extra']['giscus'] = info['giscus']
     template['nav'] = toc.get_site_nav()
     with open('mkdocs.yml', 'w', encoding='utf-8') as file:
         yaml.dump(template, file, allow_unicode=True, indent=4, sort_keys=False)
